09c5b239c5fa86058ecf0aea26b6adde6f10e987
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-07-17 15:15:12 +0530

    adding and updating files

---
+++ Strings/introStrings.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // char str[]= {'a', 'b', 'c', 'd', 'e', 'f'};
+    // for(int i=0;str[i]!='\0';i++){
+    //     cout<<str[i]<<" ";
+    // }
+
+    // for(int i=0;str[i];i++){
+    //     cout<<str[i]<<" ";
+    // }
+
+    // char str[5]= "abcd";
+    // for(int i=0;i<5;i++){
+    //     cout<<str[i]<<" ";
+    // }
+    // cout<<endl;
+    // cout<<(int)(str[1])<<endl;
+    // cout<<(int)(str[6]);
+      //str.length()  //str.size()
+
+      string name;
+      getline(cin, name);    //it will take space also between the input strings
+      cout<<name;
+
+
+
+
+}
