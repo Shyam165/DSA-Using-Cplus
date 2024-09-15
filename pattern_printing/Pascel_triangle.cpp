55c41cbe58912632f2eebe10fd2b5aee3dbff6b4
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-09-15 18:25:33 +0530

    adding pattern printings files

---
+++ pattern_printing/Pascel_triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // optimised code
+    for (int i = 0; i <= n; i++)
+    {
+        int curr = 1;
+        for (int j = 0; j <= i; j++)
+        {
+
+            cout << curr << " ";
+            curr = curr * (i - j) / (j + 1); // cout<<fact(i)/fact(j)*fact(i-j);
+        }
+        cout << endl;
+    }
+    return 0;
+}
