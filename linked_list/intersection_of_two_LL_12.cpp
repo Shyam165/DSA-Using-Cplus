37db08c2b915adbc2f6c2ce47fc97d17b43db47a
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-09-18 08:50:15 +0530

    adding and updating linked list files

---
+++ linked_list/intersection_of_two_LL_12.cpp
@@ -0,0 +1,43 @@
+//LEETCODE 160 PROBLEM NUMBER
+
+
+// ListNode* getIntersectionNode(ListNode* headA, ListNode* headB){
+//     ListNode* tempA = headA;
+//     ListNode* tempB = headB;
+//     int lenA = 0;
+//     while(tempA!=NULL){
+//         lenA++;
+//         tempA = tempA->next;
+//     }
+//     int lenB=0;
+//     while(tempB!=NULL){
+//         lenB++;
+//         tempB = tempB->next;
+//     }
+
+//     tempA = headA;
+//     tempB = headB;
+
+//     if(lenA>lenB){
+//         int diff = lenA - lenB;
+//         for(int i=1;i<=diff;i++){
+//             tempA = tempA->next;
+//         }
+//         while(tempA!=tempB){
+//             tempA = tempA->next;
+//             tempB=temp->next;
+//         }
+//         return tempA;
+//     }
+//     else {
+//         int diff = lenB - lenA;
+//         for(int i=1;i<=diff;i++){
+//             tempB = tempB->next;
+//         }
+//         while(tempA!=tempB){
+//             tempA = tempA->next;
+//             tempB=temp->next;
+//         }
+//         return tempA;
+//     }
+// }
--- linked_list/remove_n_node_from_end_11.cpp
+++ linked_list/remove_n_node_from_end_11.cpp
@@ -32,26 +32,24 @@ class Solution{
 
 
 //   ListNode* middleNode(ListNode* head, int n){
+ListNode* slow = head;
+ListNode* fast = head;
+for(int i=1;i<=n+1;i++){
+    if(fast==NULL) return head->next;
+    fast=fast->next;
+}
 
-//     int len=0;
-//     ListNode* temp = head;
-//     while(temp!=NULL){
-//         len++;
-//         temp=temp->next;
-//     }
-//     if(n==len){
-//         head=head->next;
-//         return head;
-//     }
+while(fast!=NULL){
+    slow = slow->next;
+    fast = fast->next;
+}
 
-//     int m = len-n+1;
-//     int idx = m-1;
-//     temp = head;
-//     for(int i=1;i<=idx-1;i++){
-//         temp=temp->next;
-//     }
-//     temp->next= temp->next->next;
-//     return head;
+slow->next = slow->next->next;
+return head;
 //   } 
+
+
+
+
 
  };
