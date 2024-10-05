73351408f49c0ae47714171f57577576a6b2fee9
Shyam Singh <2021pietcsshyam165@poornima.org>
2024-10-05 10:48:36 +0530

    adding leetcode problem solution files

--- Arrays and Vectors/kadane_algo.cpp
+++ Arrays and Vectors/kadane_algo.cpp
@@ -5,14 +5,26 @@
 using namespace std;
 
 int main(){
-    int arr[5]={1,2,3,4,5};
+    int arr[7]={3,-4,5,4,-1,7,-8};
     int maxsum=INT32_MIN;
-    int curr;
-    for(int st=0;st<5;st++){
-        curr=0;
-        for(int end=st;end<5;end++){
-          curr+=arr[end];
-          maxsum= max(curr, maxsum);
+    int curr=0;
+
+
+    // for(int st=0;st<5;st++){
+    //     curr=0;
+    //     for(int end=st;end<5;end++){
+    //       curr+=arr[end];
+    //       maxsum= max(curr, maxsum);
+    //     }
+    // }
+
+
+    //best approach to solve this problem
+    for(int i=0;i<7;i++){
+        curr+=arr[i];
+        maxsum=max(curr, maxsum);
+        if(curr<0){
+            curr=0;
         }
     }
     cout<<"the maximum sum of subarray is "<<maxsum<<endl;
